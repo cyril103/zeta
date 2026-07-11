030acb3b530e4cd60184a4d9c5785bf72ceba9ec
cyril103 <cyril10003.fr@gmail.com>
2026-07-12 00:00:01 +0200

    feat: implement io.println for strings

--- runtime/io.asm
+++ runtime/io.asm
@@ -9,6 +9,25 @@ public zeta_fn_io__println
 zeta_fn_io__print:
     mov rsi, qword [rsp+8]
     mov rdx, qword [rsp+16]
+    jmp io_write_all
+
+zeta_fn_io__println:
+    mov rsi, qword [rsp+8]
+    mov rdx, qword [rsp+16]
+    call io_write_all
+    test rax, rax
+    js .return
+    mov r9, rax
+    lea rsi, [io_newline]
+    mov edx, 1
+    call io_write_all
+    test rax, rax
+    js .return
+    add rax, r9
+.return:
+    ret
+
+io_write_all:
     xor r8d, r8d
 .write:
     test rdx, rdx
@@ -30,6 +49,5 @@ zeta_fn_io__print:
     mov rax, r8
     ret
 
-zeta_fn_io__println:
-    xor eax, eax
-    ret
+section '.rodata'
+io_newline: db 10
